dd0efda7ab61c7d5c65ab1945d9cc5425f27388b
hesen <hesen123@gmail.com>
2026-06-01 15:03:22 +0400

    affa

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -3,14 +3,14 @@ def add(a, b):
     """Add two numbers."""
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     """Subtract two numbers."""
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     """Multiply two numbers."""
     return a * b
 
-def divide(a, b):
+def div(a, b):
     """Divide two numbers."""
     return a / b
