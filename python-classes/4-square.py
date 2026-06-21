6d873b1bddb31f05fbcb4d7ab4396c9f4743e803
hesen <hesen123@gmail.com>
2026-06-21 13:59:18 +0400

    ssdsd

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
