677fffaccc1cbbdb262097b867347312c25e2c7e
hesen <hesen123@gmail.com>
2026-06-21 22:22:10 +0400

    gjgjjj

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Defines an empty Rectangle class."""
+
 
 class Rectangle:
-    """An empty class that defines a rectangle."""
+    """An empty class that represents a rectangle."""
     pass
