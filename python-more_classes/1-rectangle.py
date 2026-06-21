ec327bda04b3e17f12407b8e6a785761a373d156
hesen <hesen123@gmail.com>
2026-06-21 22:26:50 +0400

    yyjjy

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,11 +7,11 @@ class Rectangle:
         self.width = width
         self.height = height
 
-
+    @property
     def width(self):
         return self.__width
 
-
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -19,11 +19,11 @@ class Rectangle:
             raise ValueError("width must be a non-negative integer")
         self.__width = value
 
-
+    @property
     def height(self):
         return self.__height
 
-
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
