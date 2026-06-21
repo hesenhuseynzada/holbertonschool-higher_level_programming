986789eaf3fc36b5b443b7016d32e9896380faa8
hesen <hesen123@gmail.com>
2026-06-21 13:58:16 +0400

    tjthth

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
