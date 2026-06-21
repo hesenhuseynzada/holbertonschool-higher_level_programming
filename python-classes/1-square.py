09c0be0b3457f2d1044be433ee21cdb63fd98d0d
hesen <hesen123@gmail.com>
2026-06-21 13:42:01 +0400

    vdvdvvdv

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""A module that defines a square."""
 class Square:
-    """This class defines a square."""
+    """A class that defines a square by its size."""
+
     def __init__(self, size):
-        self.size = size
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the new square (no type/value verification).
+        """
+        self.__size = size
